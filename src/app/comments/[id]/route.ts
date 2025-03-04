55a973ae1aaa911820dd23cc00be5022696c0bd4
Techn0buddy <jaisingh8854@gmail.com>
2025-03-04 18:36:51 +0530

    route handlers done

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation"
 import { comments } from "../data"
 
 export async function GET(_request: Request,
@@ -6,8 +7,9 @@ export async function GET(_request: Request,
         id: string,
     }
 }) {
-    const comment = comments.find((comment) => comment.id === parseInt(params?.id));
-    return Response.json(comment);
+    const index = comments.findIndex(comment => comment.id === parseInt(params.id));
+    if (index === -1) redirect("/comments");
+    return Response.json(comments[index]);
 }
 
 export async function PATCH(request: Request,
@@ -19,8 +21,24 @@ export async function PATCH(request: Request,
     const body = await request.json();
     const { text } = body;
     const index = comments.findIndex(comment => comment.id === parseInt(params.id));
+    if (index === -1) return new Response("comment not found", { status: 404 });
     comments[index].text = text;
 
     return Response.json(comments[index]);
+
+}
+
+export async function DELETE(request: Request,
+    {params}:{params : {id:string}}
+) {
+    const index = comments.findIndex(comment => {
+        return comment.id === parseInt(params.id);
+    });
+    if (index === -1) {
+    return new Response("comment not found", { status: 404 });
+  }
+    const deletedComment = comments[index];
+    comments.splice(index, 1);
+    return Response.json(deletedComment);
 
 }
--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,10 @@
-export async function GET() {
+import { headers } from "next/headers";
+import { type NextRequest } from "next/server";
+
+export async function GET(request:NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    const headerList = await headers();
+    console.log(requestHeaders.get("Authorization"));
+    console.log(headerList.get("Authorization"))
     return new Response("Profile API!!");
 }
